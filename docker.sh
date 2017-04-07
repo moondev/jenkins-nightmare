1b4a4e368f4642950df95af9a80a2f150bc4fddb
Chad Moon <chad.d.moon@gmail.com>
2017-04-07 12:46:41 -0600

    blue ocean

--- docker.sh
+++ docker.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+
+
+echo "building jenkins container with most recent war"
 docker build -t jenkins-nightmare .
 
 docker kill jenkins-nightmare
 docker rm jenkins-nightmare
 docker run -d --name jenkins-nightmare -p 4444:8080 jenkins-nightmare
 
-sleep 10
+echo "waiting for jenkins to bootstrap"
+
+sleep 15
 
 npm install nightmare
+
+
 
 TOKEN=`docker exec jenkins-nightmare cat /root/.jenkins/secrets/initialAdminPassword`
 
